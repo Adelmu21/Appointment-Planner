9e47fa4910673a48ec6972f63683cce9f70499ed
Adel Moraywed <adel-u21@outlook.com>
2023-08-27 21:06:27 -0600

    added json packs

--- appointment-planner-starting-v18/src/containers/appointmentsPage/AppointmentsPage.js
+++ appointment-planner-starting-v18/src/containers/appointmentsPage/AppointmentsPage.js
@@ -12,6 +12,7 @@ export const AppointmentsPage = ({ appointments, contacts, addAppointments }) =>
  const [ contact, setContact ] = useState("");
  const [ date, setDate ] = useState("");
  const [ time, setTime ] = useState("");
+ const [ title, setTitle ] = useState("");
 
 
 
@@ -21,13 +22,14 @@ export const AppointmentsPage = ({ appointments, contacts, addAppointments }) =>
     Add contact info and clear data  
     */
     // Add a new appointment using the callback function
-    addAppointment(name, contact, date, time);
+    addAppointments(title, name, contact, date, time);
 
     // Clear form data
     setName("");
     setContact("");
     setDate("");
     setTime("");
+    setTitle("");
   };
 
   return (
@@ -35,6 +37,18 @@ export const AppointmentsPage = ({ appointments, contacts, addAppointments }) =>
       <section>
         <h2>Add Appointment</h2>
         <AppointmentForm
+          // name={name}
+          // setName={setName}
+          // contact={contact}
+          // setContact={setContact}
+          // date={date}
+          // setDate={setDate}
+          // time={time}
+          // setTime={setTime}
+          // handleSubmit={handleSubmit}
+          // contacts={contacts}
+          title={title} // Pass the title state here
+          setTitle={setTitle}
           name={name}
           setName={setName}
           contact={contact}
